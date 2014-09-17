iquidus/explorer

commit b53fc50164fdf946e0cd39bf8feff588753611b6

skip invalid vouts & merge duplicate addresses

lib/database.js
@@ -27,8 +27,24 @@ function prepare_vout(vout, txid, cb) {
   var arr_vout = [];
   lib.syncLoop(vout.length, function (loop) {
     var i = loop.iteration();
-    arr_vout.push({addresses: vout[i].scriptPubKey.addresses, amount: vout[i].value, index: vout[i].n});
-    loop.next();
+    // make sure vout has an address
+    if (vout[i].scriptPubKey.addresses) { 
+      // check if vout address is unique, if so add it array, if not at its amount to existing index
+      is_unique(arr_vout, vout[i].scriptPubKey.addresses[0], function(unique, index) {
+        if (unique == true) {
+          // unique vout
+          arr_vout.push({addresses: vout[i].scriptPubKey.addresses[0], amount: vout[i].value});
+          loop.next();
+        } else {
+          // already exists
+          arr_vout[index].amount = parseFloat(arr_vout[index].amount) + parseFloat(vout[i].value);
+          loop.next();
+        }
+      });
+    } else {
+      // no address, move to next vout
+      loop.next();
+    }
   }, function(){
     return cb(arr_vout);
   });

0x01050/Iquidus-explorer

commit 1e729e0c4c4da7846c9bb3857a8951890023f277

CRUCIAL: vout address index fix; broken from earlier commit: b53fc50164fdf946e0cd39bf8feff588753611b6

lib/database.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose')
   , Markets = require('../models/markets')
   , Address = require('../models/address')
   , Tx = require('../models/tx')
+  , Richlist = require('../models/richlist')
   , lib = require('./explorer')
   , settings = require('./settings')
   , mintpal = require('./mintpal')
@@ -33,7 +34,7 @@ function prepare_vout(vout, txid, cb) {
       is_unique(arr_vout, vout[i].scriptPubKey.addresses[0], function(unique, index) {
         if (unique == true) {
           // unique vout
-          arr_vout.push({addresses: vout[i].scriptPubKey.addresses[0], amount: vout[i].value});
+          arr_vout.push({addresses: vout[i].scriptPubKey.addresses, amount: vout[i].value});
           loop.next();
         } else {
           // already exists
@@ -60,6 +61,10 @@ function find_address(hash, cb) {
   });
 }
 
+function richlist(address, received, sent, cb) {
+
+}
+
 function update_address(hash, txid, amount, type, cb) {
   find_address(hash, function(address) {
     if (address) {
@@ -79,6 +84,7 @@ function update_address(hash, txid, amount, type, cb) {
           } else {
             received = received + amount;
           }
+          // call rich list function here **********
           //update db entry
           Address.update({a_id:hash}, {
             txs: tx_array,
@@ -462,7 +468,7 @@ module.exports = {
       if (err) {
         return cb(err);
       } else {
-        console.log('tx stored: %s', txid);
+        //console.log('tx stored: %s', txid);
         return cb();
       }
     });
@@ -475,7 +481,7 @@ module.exports = {
         if (err) {
           loop.next();
         } else {
-          console.log('tx stored: %s', block.tx[i]);
+          //console.log('tx stored: %s', block.tx[i]);
           loop.next();
         }
       });
@@ -497,12 +503,13 @@ module.exports = {
         return cb();
       } else {
         console.log("initial markets entry created for %s", market);
-        console.log(newMarkets);
+        //console.log(newMarkets);
         return cb();
       }
     });
   },
   
+  // checks market data exists for given market
   check_market: function(market, cb) {
     Markets.findOne({market: market}, function(err, exists) {
       if(exists) {
@@ -513,6 +520,7 @@ module.exports = {
     });
   },
 
+  // gets market data for given market
   get_market: function(market, cb) {
     Markets.findOne({market: market}, function(err, data) {
       if(data) {
@@ -523,6 +531,34 @@ module.exports = {
     });
   },
 
+  // creates initial richlist entry in database; called on first launch of explorer
+  create_richlist: function(coin, cb) {
+    var newRichlist = new Richlist({
+      coin: coin,
+    });
+    newRichlist.save(function(err) {
+      if (err) {
+        console.log(err);
+        return cb();
+      } else {
+        console.log("initial richlist entry created for %s", coin);
+        console.log(newRichlist);
+        return cb();
+      }
+    });
+  },
+  // checks richlist data exists for given coin
+  check_richlist: function(coin, cb) {
+    Richlist.findOne({coin: coin}, function(err, exists) {
+      if(exists) {
+        return cb(true);
+      } else {
+        return cb(false);
+      }
+    });
+  },
+
+  // updates market data for given market; called by sync.js
   update_markets_db: function(market, cb) {
     if(market == 'mintpal') {
       mintpal_get_chartdata(function(chartdata){
@@ -570,7 +606,8 @@ module.exports = {
       return cb(false);
     }
   },
-
+  
+  // updates stats data for given coin; called by sync.js
   update_db: function(coin, cb) {
     lib.get_blockcount( function (count) {
       lib.get_hashrate( function (hashrate) {
@@ -598,17 +635,8 @@ module.exports = {
                     connections: connections,
                   }, function() {
                     return cb(true);
-                      /*Stats.findOne({coin: settings.coin}, function(err, stats) {
-                        if(stats) {
-                          txarray = [];
-                          update_txarray(stats.transactions, 0);
-                          
-                        }
-                      });*/
-
-                    });
                   });
-                
+                });
               } else {
                 return cb(false);
               }
@@ -619,6 +647,7 @@ module.exports = {
     });
   },
 
+  // updates tx, address & richlist db's; called by sync.js
   update_tx_db: function(coin, start, end, timeout, cb) {
     var complete = false;
     lib.syncLoop((end - start) + 1, function (loop) {      